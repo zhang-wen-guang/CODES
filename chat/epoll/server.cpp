1f15de787b6179c59059144137868925e902bf48
zhangwg1990 <zhangwg1990@gmail.com>
2016-07-06 14:47:05 +0800

    chat cpp

--- chat/epoll/server.cpp
+++ chat/epoll/server.cpp
@@ -7,14 +7,24 @@
 
 #include <iostream>
 #include <sys/socket.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
 
+#define MAXSIZE     1024
+#define LISTENQ     5
+#define FDSIZE      1000
+#define EPOLLEVENTS 100
+
+/* 创建server端的socket, 并将其与server端地址绑定 */
 int create_socket(int port) {
     int server_socket;
 
     /* 创建server端socket号 */
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == server_socket) {
-        std::cout<<"Failed to create server socket"<<std::end;
+        std::cout<<"Failed to create server socket"<<std::endl;
         exit(1);
     }
 
@@ -38,13 +48,126 @@ int create_socket(int port) {
     if (-1 == bind_retval) {
         std::cout<<"Failed to bind()" <<std::endl;
     }
+    return server_socket;
+}
+
+static void add_event(int epollfd,int fd,int state){
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+}
+
+
+//处理接收到的连接
+static void handle_accpet(int epollfd,int listenfd){
+     int clifd;     
+     struct sockaddr_in cliaddr;     
+     socklen_t  cliaddrlen;     
+     clifd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen);     
+     if (clifd == -1)         
+     perror("accpet error:");     
+     else {         
+         printf("accept a new client: %s:%d\n",inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);                       //添加一个客户描述符和事件         
+         add_event(epollfd,clifd,EPOLLIN);     
+     } 
+}
+
+//删除事件
+static void delete_event(int epollfd,int fd,int state) {
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,fd,&ev);
+}
+
+//修改事件
+static void modify_event(int epollfd,int fd,int state){     
+    struct epoll_event ev;
+    ev.events = state;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&ev);
+}
+
+//读处理
+static void do_read(int epollfd,int fd,char *buf){
+    int nread;
+    nread = read(fd,buf,MAXSIZE);
+    if (nread == -1)     {         
+        perror("read error:");         
+        close(fd); //记住close fd        
+        delete_event(epollfd,fd,EPOLLIN); //删除监听 
+    }
+    else if (nread == 0)     {         
+        fprintf(stderr,"client close.\n");
+        close(fd); //记住close fd       
+        delete_event(epollfd,fd,EPOLLIN); //删除监听 
+    }     
+    else {         
+        printf("read message is : %s",buf);        
+        //修改描述符对应的事件，由读改为写         
+        modify_event(epollfd,fd,EPOLLOUT);     
+    } 
+}
+
+//写处理
+static void do_write(int epollfd,int fd,char *buf) {     
+    int nwrite;     
+    nwrite = write(fd,buf,strlen(buf));     
+    if (nwrite == -1){         
+        perror("write error:");        
+        close(fd);   //记住close fd       
+        delete_event(epollfd,fd,EPOLLOUT);  //删除监听    
+    }else{
+        modify_event(epollfd,fd,EPOLLIN); 
+    }    
+    memset(buf,0,MAXSIZE); 
+}
+
+//事件处理函数
+static void handle_events(int epollfd,struct epoll_event *events,int num,int listenfd,char *buf)
+{
+     int i;
+     int fd;
+     //进行遍历;这里只要遍历已经准备好的io事件。num并不是当初epoll_create时的FDSIZE。
+     for (i = 0;i < num;i++)
+     {
+         fd = events[i].data.fd;
+        //根据描述符的类型和事件类型进行处理
+         if ((fd == listenfd) &&(events[i].events & EPOLLIN))
+            handle_accpet(epollfd,listenfd);
+         else if (events[i].events & EPOLLIN)
+            do_read(epollfd,fd,buf);
+         else if (events[i].events & EPOLLOUT)
+            do_write(epollfd,fd,buf);
+     }
 }
 
 int main()
 {
     int server_socket;
+    int port;
+    int epollfd;
+    int ret;
+    char buf[1024];
 
+    port = 8765;
+    server_socket = create_socket(port);
 
-    std::cout<<"Hello"<<std::endl;
+    /* 监听 */
+    listen(server_socket, 10);
+
+    struct epoll_event events[100];
+
+    epollfd = epoll_create(1024);
+
+    add_event(epollfd,server_socket,EPOLLIN);
+
+    while(1) {
+        //该函数返回已经准备好的描述符事件数目
+        ret = epoll_wait(epollfd,events,EPOLLEVENTS,-1);
+        //处理接收到的连接
+        handle_events(epollfd,events,ret,server_socket,buf);
+    }
     return 0;
 }
