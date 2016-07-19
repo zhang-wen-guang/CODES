1ecff5f157e6d30b9557547c2096ad196455a233
ZWG <zhangwg1990@gmail.com>
2016-07-19 23:22:34 +0800

    epoll

--- chat/epoll2/server.cpp
+++ chat/epoll2/server.cpp
@@ -9,10 +9,22 @@
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
+
+int set_socket_nonblocking(int sfd) {
+    int flags;
+
+    flags = fcntl(sfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(sfd, F_SETFL, flags);
+    return 0;
+}
 
 int main()
 {
@@ -35,13 +47,39 @@ int main()
     /* 绑定完了就该listen了 */
     listen(sfd, 10);
 
-    int tcp_no = accept(sfd, (struct sockaddr *)NULL, NULL);
-    cout << "tcp_no = " << tcp_no << endl;
-    char s1[1024];
-    while (recv(tcp_no, s1, 1000, 0)) {
-        cout << s1 << endl;
-    }
+    /* 将socket fd设置为nonblocking模式 */
+    set_socket_nonblocking(sfd);
 
-    cout<<"Hello, I am server"<<endl;
+    /* 开始搞epoll那一套 */
+    int efd = epoll_create1(0);
+
+    struct epoll_event event;
+    struct epoll_event *events;
+
+    /* 注册第一个事件, 就是sfd监听 */
+    event.data.fd = sfd;
+    event.events = EPOLLIN | EPOLLET;
+    
+    epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event);
+
+    int maxsize = 100;
+
+    calloc(100, sizeof(event));
+
+    /* 准备工作做完了, 开始主循环 */
+    int n;
+    while (1) {
+        n = epoll_wait(efd, events, maxsize, -1);
+        /* 有n个事件响应了, 对这n个事件处理吧 */
+        for (int i = 0; i < n; ++i) {
+            if ((events[i].events & EPOLLERR) ||
+                (events[i].events & EPOLLHUP) ||
+                (!(events[i].events & EPOLLIN))) {
+                cout << "epoll err" << endl;
+                close(events[i].data.fd);
+            }
+        }
+    }
+    
     return 0;
 }
