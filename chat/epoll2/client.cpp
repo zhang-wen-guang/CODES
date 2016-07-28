b0436f2f7ad2ff7f5d844ce82ff1f384d7c758fe
zhangwg1990 <zhangwg1990@gmail.com>
2016-07-28 20:23:56 +0800

    epoll2

--- chat/epoll2/client.cpp
+++ chat/epoll2/client.cpp
@@ -5,11 +5,60 @@
     > Created Time: 2016年07月18日 星期一 22时33分23秒
  ************************************************************************/
 
+/* 想了一下, client还是需要多线程实现 */
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
 using namespace std;
 
-int main()
+void* send_msg(void * a_sfd) {
+    int sfd = *(int*)a_sfd;
+    char buf[1024];
+
+    while (cin.getline(buf, 1024, '\n')) {
+        send(sfd, buf, strlen(buf), 0);
+    }
+    close(sfd);
+    exit(0);
+}
+
+int main(int argc, char **argv)
 {
-    cout<<"Hello, I am client" << endl;
+    /* 同样, 第一步弄一个socket号 */
+    int sfd;
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* 然后bind */
+    struct sockaddr_in server_addr;
+
+    /* 注意, 这里用的还是server的地址和prot */
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(8765);
+    inet_pton(AF_INET, argv[1], &(server_addr.sin_addr));
+
+    /* 现在开始connect */
+    connect(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    /* 发送名字 */
+    send(sfd, argv[2], strlen(argv[2]), 0);
+
+    /* 创建一个新的线程用于处理发送信息 */
+    pthread_t pth_send;
+    pthread_create(&pth_send, NULL, send_msg, &sfd);
+
+    /* 处理收到的消息 */
+    int len_recv;
+    char buf[1024];
+    while (len_recv = recv(sfd, buf, 1024, 0)) {
+        buf[len_recv] = '\0';
+        printf("%s", buf);
+    }
+    
     return 0;
 }
--- chat/epoll2/server.cpp
+++ chat/epoll2/server.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
@@ -14,6 +15,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+
+#define MAX_MESSAGE  2048
+#define MAX_NAME     100
 
 using namespace std;
 
@@ -24,6 +29,55 @@ int set_socket_nonblocking(int sfd) {
     flags |= O_NONBLOCK;
     fcntl(sfd, F_SETFL, flags);
     return 0;
+}
+
+bool is_blank(char *s) {
+    for (int i = 0; i < strlen(s); ++i) {
+        if (!isspace(s[i]) && '\n' != s[i]) return false;
+    }
+    return true;
+}
+
+void pack_message_and_send(int sfd, map<int, string> clients2, char *buf) {
+    char message[MAX_MESSAGE] = "";
+
+    strcat(message, clients2[sfd].c_str());
+    strcat(message, " : ");
+    strcat(message, buf);
+    strcat(message, "\n");
+    for (auto pair : clients2) {
+        if (pair.first != sfd)
+            send(pair.first, message, strlen(message), 0);
+    }
+}
+
+void welcome(string name, map<int, string> clients2) {
+    char message[MAX_MESSAGE] = "";
+    strcat(message, "系统提示 : ");
+    strcat(message, name.c_str());
+    strcat(message, "加入了聊天室\n");
+    for (auto pair : clients2) {
+        send(pair.first, message, strlen(message), 0);
+    }
+}
+
+void quit(int sfd, map<string, int> &clients, map<int, string> &clients2) {
+    int to_sfd;
+    string name = clients2[sfd];
+    char message[MAX_MESSAGE] = "";
+
+    /* 从两个map中删除这个client */
+    clients.erase(name);
+    clients2.erase(sfd);
+
+    /* 制作message */
+    strcat(message, "系统提示 : ");
+    strcat(message, name.c_str());
+    strcat(message, "离开了聊天室\n");
+
+    for (auto pair : clients2) {
+        send(pair.first, message, strlen(message), 0);
+    }
 }
 
 int main()
@@ -38,8 +92,8 @@ int main()
     memset(&server_addr, 0, sizeof(struct sockaddr_in));
 
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port        = htons(8765);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int bind_val = bind(sfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr_in));
     if (-1 == bind_val) cout << "Failed to bind" << endl;
@@ -64,10 +118,15 @@ int main()
 
     int maxsize = 100;
 
-    calloc(100, sizeof(event));
+    events = (struct epoll_event*)calloc(100, sizeof(event));
 
     /* 准备工作做完了, 开始主循环 */
     int n;
+    map<string, int> clients;
+    map<int, string> clients2;
+    string client_name;
+    char buf[MAX_MESSAGE];
+    int count;
     while (1) {
         n = epoll_wait(efd, events, maxsize, -1);
         /* 有n个事件响应了, 对这n个事件处理吧 */
@@ -76,13 +135,27 @@ int main()
             if ((events[i].events & EPOLLERR) ||
                 (events[i].events & EPOLLHUP) ||
                 (!(events[i].events & EPOLLIN))) {
-                cout << "epoll err" << endl;
+
+                cout << "name = " <<  clients2[events[i].data.fd] << endl;
                 close(events[i].data.fd);
             } else if (events[i].data.fd == sfd) {
                 /* 当event.data.fd=sfd时 */
                 /* 我理解, 这里需要做2件事, 1, accept. 2, 注册新的事件 */
                 int infd;
                 infd = accept(sfd, (struct sockaddr *)NULL, NULL);
+
+                /* 拿到client的名字 */
+                count = recv(infd, buf, MAX_MESSAGE, 0);
+                buf[count] = '\0';
+                client_name = buf;
+
+                /* 发送欢迎信息, 注意不能放到后面 */
+                welcome(client_name, clients2);
+                
+                /* 将这个新的client放到clients中去 */
+                clients[client_name] = infd;
+                clients2[infd] = client_name;
+                /* TODO : 重名问题后面解决 */
 
                 /* 注册事件 */
                 set_socket_nonblocking(infd);
@@ -91,10 +164,18 @@ int main()
                 epoll_ctl(efd, EPOLL_CTL_ADD, infd, &event);
             } else {
                 /* 收到消息, 打印消息就行了 */
-                
+                int recv_sfd = events[i].data.fd;
+                count = read(recv_sfd, buf, MAX_MESSAGE);
+                buf[count] = '\0';
+                if (0 == count) {
+                    quit(recv_sfd, clients, clients2);
+                }
+                if (is_blank(buf)) continue;
+                pack_message_and_send(recv_sfd, clients2, buf);
             }
         }
     }
-    
+    free(events);
+    close(sfd);
     return 0;
 }
